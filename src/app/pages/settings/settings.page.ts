210ce6d9d563a057605136a7b7a025bcdcb2a0c4
Last <guygamerxd@gmail.com>
2026-09-24 22:13:38 -0500

    build: compile fresh Android APK v1.4.3 with all latest features and sync cache

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -50,7 +50,7 @@ export class SettingsPage implements OnInit, OnDestroy {
   deviceTimezone = '';
   localResetTime = '';
 
-  versionNumber = '1.4.2';
+  versionNumber = '1.4.3';
   appVersion = `v${this.versionNumber}`;
   apkFileName = 'lastcodex_stable.apk';
   apkDownloadUrl = `assets/${this.apkFileName}`;
--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lastresources-v1.3.0';
+const CACHE_NAME = 'lastresources-v1.4.3';
 
 const PRECACHE_URLS = [
   './',
